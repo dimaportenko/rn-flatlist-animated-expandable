c5f267de440e8be4d802fb7eef0b6e47625e7cb5
Dmytro Portenko <troublesohard@gmail.com>
2020-05-28 21:42:11 +0300

    transition animation

--- src/ExpandableCellList.js
+++ src/ExpandableCellList.js
@@ -2,28 +2,37 @@
  * @flow
  * Created by Dima Portenko on 28.05.2020
  */
-import React from 'react';
+import React, { useRef } from 'react';
 import { View, FlatList, Image, Text } from 'react-native';
 import { getData } from './getData';
 import { ListItem } from './ListItem';
+import { Transition, Transitioning } from 'react-native-reanimated';
 
 export const ExpandableCellList = () => {
+  const transitionRef = useRef();
+  const transition = <Transition.Change interpolation="easeInOut" />;
   const data = getData();
-  console.warn(data)
+
+  const onPress = () => {
+    transitionRef.current.animateNextTransition();
+  }
 
   const renderItem = ({ item }) => {
     return (
-      <ListItem item={item} />
+      <ListItem
+        item={item}
+        onPress={onPress}
+      />
     )
   }
 
   return (
-    <View style={{ flex: 1 }}>
+    <Transitioning.View ref={transitionRef} transition={transition} style={{ flex: 1 }}>
       <FlatList
         data={data}
         keyExtractor={(item, index) => `${item.title}${index}`}
         renderItem={renderItem}
       />
-    </View>
+    </Transitioning.View>
   );
 };
